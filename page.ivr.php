0591734a3b4f42794e4919e47a261e4babd24b6c
Moshe Brevda <mbrevda@gmail.com>
2010-11-08 16:05:12 +0000

    re #1798 / r9235 - fix function call so that it actually dose soemthing

--- page.ivr.php
+++ page.ivr.php
@@ -429,7 +429,7 @@ function drawdestinations($count, $sel,  $dest, $ivr_ret) {
 		<small><a href="#" class="info"><?php echo _("Return to IVR")?><span><?php echo _("Check this box to have this option return to a parent IVR if it was called from a parent IVR. If not, it will go to the chosen destination.<br><br>The return path will be to any IVR that was in the call path prior to this IVR which could lead to strange results if there was an IVR called in the call path but not immediately before this")?></span></a></small>
 		<input type="checkbox" name="ivr_ret<?php echo $count ?>" value="ivr_ret" <?php echo $ivr_ret?'CHECKED':''; ?>>
 	<?php if(function_exists('ivr_dests_hook_show')){
-		ivr_dests_hook_show($id, $dest);
+		echo ivr_dests_hook_show($id, $sel);
 	}
 	?>
 		<img src="images/trash.png" style="cursor:pointer" title="<?php echo _('Delete this entry. Dont forget to click \"Save\" to save changes!');?>" onclick="delEntry(<?php echo $count;?>)">
