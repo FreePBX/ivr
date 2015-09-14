d1ea3ccdb1c8b2eca81499f2760515bce9f7424d
Andrew Nagy <andrew.nagy@the159.com>
2015-09-14 11:44:55 -0700

    FREEPBX-10209 htmlentities need to use UTF-8

--- Ivr.class.php
+++ Ivr.class.php
@@ -20,6 +20,7 @@ class Ivr extends \FreePBX_Helpers implements \BMO {
 	}
 
 	public function getDetails($id = false) {
+		$s = ini_get("default_charset");
 		$sql = 'SELECT * FROM ivr_details';
 		if ($id) {
 			$sql .= ' where  id = :id ';
@@ -30,14 +31,14 @@ class Ivr extends \FreePBX_Helpers implements \BMO {
 		$sth->execute(array(":id" => $id));
 		$res = $sth->fetchAll();
 		if ($id && isset($res[0])) {
-			$res[0]['name'] = htmlentities(stripslashes($res[0]['name']));
-			$res[0]['description'] = htmlentities(stripslashes($res[0]['description']));
+			$res[0]['name'] = htmlentities($res[0]['name'],ENT_COMPAT | ENT_HTML401, "UTF-8");
+			$res[0]['description'] = htmlentities($res[0]['description'],ENT_COMPAT | ENT_HTML401, "UTF-8");
 			return $res[0];
 		} else {
 			$res = is_array($res)?$res:array();
 			foreach ($res as $key => $value) {
-				$res[$key]['name'] = htmlentities(stripslashes($res[$key]['name']));
-				$res[$key]['description'] = htmlentities(stripslashes($res[$key]['description']));
+				$res[$key]['name'] = htmlentities($res[$key]['name'],ENT_COMPAT | ENT_HTML401, "UTF-8");
+				$res[$key]['description'] = htmlentities($res[$key]['description'],ENT_COMPAT | ENT_HTML401, "UTF-8");
 			}
 			return $res;
 		}
