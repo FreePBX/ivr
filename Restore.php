900eec2495dfa01bfb45c3a60e77d797b2989c63
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 17:34:11 -0500

    [Module Tag script: ivr 15.0.14] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,38 +2,18 @@
 namespace FreePBX\modules\Ivr;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    if(empty($config)){
-	echo _("Backup empty").PHP_EOL;
-	return;
-    }
-    foreach ($configs['ivrs'] as $ivr) {
-        $this->FreePBX->Ivr->saveDetails($ivr);
-    }
-    foreach($configs['entries'] as $id => $entries){
-        $this->FreePBX->Ivr->saveEntries($id, $enrties);
-    }
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-      $tables = array_flip($tables+$unknownTables);
-      if(!isset($tables['ivr_entries'])){
-          return $this;
-      }
-      $bmo = $this->FreePBX->Ivr;
-      $bmo->setDatabase($pdo);
-      $configs = [
-        'ivrs' => $this->FreePBX->Ivr->getDetails(),
-        'entries' => $this->FreePBX->Ivr->getAllEntries(),
-      ];
-      $bmo->resetDatabase();
-      foreach ($configs['ivrs'] as $ivr) {
-        $bmo->saveDetails($ivr);
-      }
-      foreach ($configs['entries'] as $id => $entries) {
-        $bmo->saveEntries($id, $enrties);
-      }
-      return $this;
-  }  
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		foreach ($configs['ivrs'] as $ivr) {
+			$this->FreePBX->Ivr->saveDetails($ivr);
+		}
+		foreach($configs['entries'] as $id => $entries){
+				$this->FreePBX->Ivr->saveEntries($id, $enrties);
+		}
+	}
+
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
 
