60f0e6e4fb00a85f41cbc572d979dfc1bef52d96
nthumann <me@n-thumann.de>
2019-08-17 21:39:12 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Ivr;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		foreach ($configs['ivrs'] as $id => $ivr) {
 			$this->FreePBX->Ivr->saveDetail($ivr['0']);
